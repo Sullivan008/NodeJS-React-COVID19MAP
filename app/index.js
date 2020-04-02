ecf9c936d655a30a9b4bbc1a5eb3d23e87ec7508
Sullivan008 <kozakpeter0424@gmail.com>
2020-04-02 16:05:29 +0200

    Install Express and Cors Modules; Used Axios module for downloaded global CSV data from this link: https://covid.ourworldindata.org/data/ecdc/full_data.csv; Separate readCovidCsv method many functions (downloadLatestCovidRawData, parseLatestCovidRawData, getCovidDataByCountriesSummarize) and remove this.

--- app/index.js
+++ app/index.js
@@ -1,20 +1,45 @@
-const axios = require('axios');
+const axios = require('axios').default;
 
 const csvParser = require('csv-parse');
 const { promisify } = require('util');
 const csvParserAsync = promisify(csvParser);
 
-async function readCovidCsv() {
-    const fileStream = require('fs');
+async function downloadLatestCovidRawData() {
+    return (await axios.get('https://covid.ourworldindata.org/data/ecdc/full_data.csv')).data;
+};
 
-    const rawData = fileStream.readFileSync(__dirname + '/app_data/full_data.csv');
-
-    const output = await csvParserAsync(rawData, {
+async function parseLatestCovidRawData(latestCovidRawData) {
+    return await csvParserAsync(latestCovidRawData, {
         delimiter: ',',
         columns: true
     });
+};
 
-    console.log(output);
+async function getCovidDataByCountriesSummarize(latestCovidData) {
+    const countriesSummarize = latestCovidData.reduce((
+        accumlator, {
+            location,
+            total_cases: totalCases,
+            total_deaths: totalDeaths
+        }) => {
+        accumlator[location] = {
+            location,
+            totalCases,
+            totalDeaths
+        }
+
+        return accumlator;
+    }, {});
+
+    return countriesSummarize;
+};
+
+async function run() {
+    const rawData = await downloadLatestCovidRawData();
+    const latestCovidData = await parseLatestCovidRawData(rawData);
+    const dataByCountriesSummarize = await getCovidDataByCountriesSummarize(latestCovidData);
+
+    console.log(dataByCountriesSummarize);
 }
 
-readCovidCsv();
+run();
