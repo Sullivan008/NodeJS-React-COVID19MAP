f9994a1222930de9554aafb584dfc5c5cedb6b59
Sullivan008 <kozakpeter0424@gmail.com>
2020-04-06 13:55:37 +0200

    Add google-maps-react and react-google maps Node Modules for Client; Implement CovidMap.js in core folder. This script generate Markers and Circlies from CovidMap initialization properties.; Implement three static data in App.js and Call and Render CovidMap with test datas.;

--- COVID19MAP-Client/src/App.js
+++ COVID19MAP-Client/src/App.js
@@ -1,7 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { covidDataByCountriesSummarizeJson } from './api';
+import CovidMap from './core/CovidMap';
+
+const places = [
+  {
+    id: 1,
+    name: "Park Slope",
+    latitude: "40.6710729",
+    longtitude: "-73.9988001",
+    circle: {
+      radius: 3000,
+      options: {
+        strokeColor: "#ff0000"
+      }
+    }
+  },
+  {
+    id: 2,
+    name: "Bushwick",
+    latitude: "40.6942861",
+    longtitude: "-73.9389312"
+  },
+  {
+    id: 3,
+    name: "East New York",
+    latitude: "40.6577799",
+    longtitude: "-73.9147716"
+  }
+];
 
 function App() {
   const [covidDataByCountriesSummarize, setData] = useState(null);
@@ -14,25 +41,15 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          { covidDataByCountriesSummarize && 
-            Object.values(covidDataByCountriesSummarize).map(({location, totalCases}) =>
-              <div>{location}: {totalCases}</div>)}
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CovidMap
+        center = {{lat: 40.64, lng: -73.96}}
+        zoom = {12}
+        places = {places}
+        googleMapURL = "https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY"
+        loadingElement = {<div style = {{height: '100%'}} />}
+        containerElement = {<div style = {{height: '800px'}}/>}
+        mapElement = {<div style = {{height: '100%'}}/>}
+      />
     </div>
   );
 }
---
+++ COVID19MAP-Client/src/core/CovidMap.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import { withGoogleMap, GoogleMap, withScriptjs, Marker, Circle } from "react-google-maps";
+
+const Map = props => {
+    return(
+        <GoogleMap defaultZoom = {props.zoom}
+                   defaultCenter = {props.center}>
+                { props.places.map(place => { return (
+                    <Fragment key = {place.id}>
+                        <Marker position = {{ lat: parseFloat(place.latitude),
+                                              lng: parseFloat(place.longtitude)} }>
+                            { place.circle && 
+                              <Circle defaultCenter = {{ lat: parseFloat(place.latitude),
+                                                         lng: parseFloat(place.longtitude) }}
+                                      radius = {place.circle.radius}
+                                      options = {place.circle.options}/> }
+                        </Marker>
+                    </Fragment>)})};
+        </GoogleMap>
+    );
+};
+
+export default withScriptjs(withGoogleMap(Map));
