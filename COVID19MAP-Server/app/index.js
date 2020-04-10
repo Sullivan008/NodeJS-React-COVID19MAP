f11f6fb27e13260de178af085654db6914832007
Sullivan008 <kozakpeter0424@gmail.com>
2020-04-10 11:41:24 +0200

    Install cron package for automatic data update; Set in the job, that run every 1 hour and download latest covid data;

--- COVID19MAP-Server/app/index.js
+++ COVID19MAP-Server/app/index.js
@@ -1,1 +1,13 @@
+const CronJob = require('cron').CronJob;
+
+const { updateCovidData } = require('./data-management');
+
+const job = new CronJob('0 */1 * * *', async () => {
+    await updateCovidData();
+
+    console.log("Data was updated successfully!");
+});
+
+job.start();
+
 require('./server');
